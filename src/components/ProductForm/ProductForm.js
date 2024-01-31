10c66b8a3f3575d5cb69ec3ad5a43e79a8f229ad
Anna Powaska <annapowaska@op.pl>
2024-01-31 08:54:57 +0100

    Add useMemo

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -3,30 +3,36 @@ import styles from './ProductForm.module.scss'
 import OptionColor from "./OptionColor/OptionColor";
 import OptionSize from "./OptionSize/OptionSize";
 import PropTypes from 'prop-types';
+import { useMemo } from "react";
 
 const ProductForm = props => {
-    const getPrice = (basePrice, currentSize) => {
-        return basePrice + currentSize.additionalPrice;
-      }
 
-    const sentOrder = (event, title, basePrice, currentSize, currentColor) => {
-      event.preventDefault();
-      console.log(`
+  const price = useMemo(() => {
+
+    const getPrice = (basePrice, currentSize) => {
+      return basePrice + currentSize.additionalPrice;
+    }
+      return getPrice(props.basePrice, props.currentSize);
+  }, [props.basePrice, props.currentSize]);
+  
+  const sentOrder = (event, title, totalPrice, currentSize, currentColor) => {
+    event.preventDefault();
+    console.log(`
         Your Order:
         Name: ${title}
-        Price: ${getPrice(basePrice, currentSize)}
+        Price: ${price}$
         Size: ${currentSize.name}
         Color: ${currentColor}
-      `);
-      props.setCurrentColor(props.colors[0]);
-      props.setCurrentSize(props.sizes[0]);
-    };
+    `);
+    props.setCurrentColor(props.colors[0]);
+    props.setCurrentSize(props.sizes[0]);
+  };
 
     return (
       <div>
         <header>
           <h2 className={styles.name} alt={props.names}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice(props.basePrice, props.currentSize)}$</span>
+          <span className={styles.price}>Price: {price}$</span>
         </header>
         <form>
         <OptionSize sizes={props.sizes} currentSize={props.currentSize} setCurrentSize={props.setCurrentSize}/>
